46cefedbcbc595e65beb94dec9e70669b4bdb87a
NorthernMan54 <19808920+NorthernMan54@users.noreply.github.com>
2025-09-28 19:39:04 -0400

    Update package-for-utm.sh

--- scripts/package-for-utm.sh
+++ scripts/package-for-utm.sh
@@ -106,11 +106,11 @@ check_plistbuddy() {
 # Function to check if the disk image exists
 check_disk_image() {
     if [[ ! -f "${DISK_PATH}" ]]; then
-        error "Disk image not found at ${DISK_PATH}"
+        error "${DISK_PATH} image not found at ${DISK_PATH}"
         exit 1
     fi
     if [[ ! "${DISK_PATH}" =~ \.(img|gcow2)$ ]]; then
-        error "Disk image must be a .img  or .qcow2 file"
+        error "${DISK_PATH} image must be a .img  or .qcow2 file"
         exit 1
     fi
     info "Disk image validated: ${DISK_PATH}"
